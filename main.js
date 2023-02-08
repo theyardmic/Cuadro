af89ae2258b83e43585bc00a961c2805ea3cb589
Strucker.eth <aberamacarleton@gmail.com>
2023-02-08 17:35:12 +0300

    Page rebranding

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Cuadro Digital Art Market Place</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the Cuadro logo to learn more
     </p>
   </div>
 `
