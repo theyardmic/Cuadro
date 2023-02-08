4b825584eca0d0a47455a33dfd21c6b78d6c98d3
Strucker.eth <aberamacarleton@gmail.com>
2023-02-08 19:54:08 +0300

    Font and layout edit

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
+  <h1>Cuadro Digital Art Market Place</h1>
+  <div class="card">
+    <button id="counter" type="button"></button>
+  </div>
+  <p class="read-the-docs">
+   We'll be live soon. <br />
+    Meanwhile, you can check out my other projects.
+  </p>
     <a href="https://b-friday.netlify.app" target="_blank">
       <img src="/merchy.png" class="logo" alt="Merchy logo" />
     </a>
     <a href="https://strucker.netlify.app" target="_blank">
-      <img src="strucker.png" class="logo vanilla" alt="JavaScript logo" />
+      <img src="strucker.png" class="logo vanilla" alt="Strucker logo" />
     </a>
-    <h1>Cuadro Digital Art Market Place</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-    We're working on the system. We'll be live soon. <br />
-      Meanwhile, you can check my other projects linked above.
-    </p>
+   
   </div>
 `
 
